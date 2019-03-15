35a8a99702b05e3237996e5f2214819a6d131500
regevbr <regevbr@gmail.com>
2019-03-15 17:35:51 +0200

    add support to auto upload large messages to s3 #6

--- src/s3Utils.ts
+++ src/s3Utils.ts
@@ -9,26 +9,49 @@ export const S3_MARKER = '__SQS_MESSAGE_S3__';
 export interface IS3Upload {
   bucket: string;
   key: string;
+  uploadSize: number;
 }
 
 export const uploadBlob = (s3: S3, bucket: string, blob: string): Promise<IS3Upload> => {
   const key = uuid();
-  return Promise.resolve({
-    bucket,
-    key,
-  });
+  const size = getSizeInBytes(blob);
+  return s3.putObject({
+    Body: blob,
+    Bucket: bucket,
+    Key: key,
+    ContentLength: size,
+  }).promise()
+    .then(() => {
+      return Promise.resolve({
+        uploadSize: size,
+        bucket,
+        key,
+      });
+    });
 };
 
 export const getBlob = (s3: S3, uploadData: IS3Upload): Promise<string> => {
-  // const key = uploadData.key;
-  // const bucket = uploadData.bucket;
-  return Promise.resolve('');
+  const key = uploadData.key;
+  const bucket = uploadData.bucket;
+  return s3.getObject({
+    Key: key,
+    Bucket: bucket,
+  }).promise()
+    .then((s3Result) => {
+      return s3Result.Body!.toString('utf-8');
+    });
 };
 
 export const deleteBlob = (s3: S3, uploadData: IS3Upload): Promise<void> => {
-  // const key = uploadData.key;
-  // const bucket = uploadData.bucket;
-  return Promise.resolve();
+  const key = uploadData.key;
+  const bucket = uploadData.bucket;
+  return s3.deleteObject({
+    Key: key,
+    Bucket: bucket,
+  }).promise()
+    .then(() => {
+      return Promise.resolve();
+    });
 };
 
 export const getMessageSize = (message: ISendMessageRequest): number => {
